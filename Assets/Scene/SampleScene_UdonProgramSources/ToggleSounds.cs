de7eb2c2597ad77eb64255c4643b2fc14dabb131
Miles Popiela <55100984+1-61Kilometers@users.noreply.github.com>
2023-10-26 00:38:27 -0400

    toggle sound

---
+++ Assets/Scene/SampleScene_UdonProgramSources/ToggleSounds.cs
@@ -0,0 +1,54 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ToggleSounds : UdonSharpBehaviour
+{
+    public AudioSource audioSource;
+    public float fadeDuration = 2.0f; // Duration for fade out in seconds
+
+    private bool isFadingOut = false;
+    private float startVolume;
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player.IsValid())
+        {
+            if (player.isLocal)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (!player.IsValid() || !player.isLocal) return;
+        StartFadeOut();
+    }
+
+    private void Update()
+    {
+        if (isFadingOut)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / fadeDuration;
+
+            if (audioSource.volume <= 0)
+            {
+                audioSource.Pause();  // Pause the audio when volume reaches zero
+                audioSource.volume = startVolume; // Reset volume for next time
+                isFadingOut = false;
+            }
+        }
+    }
+
+    private void StartFadeOut()
+    {
+        if (!isFadingOut)
+        {
+            isFadingOut = true;
+            startVolume = audioSource.volume;
+        }
+    }
+}
