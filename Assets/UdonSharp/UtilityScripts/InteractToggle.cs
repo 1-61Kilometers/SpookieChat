53067d706eb3ca7107e2cb72922cc0b8303df947
Miles Popiela <55100984+1-61Kilometers@users.noreply.github.com>
2023-10-25 22:16:38 -0400

    push

--- Assets/UdonSharp/UtilityScripts/InteractToggle.cs
+++ Assets/UdonSharp/UtilityScripts/InteractToggle.cs
@@ -12,14 +12,14 @@ namespace UdonSharp.Examples.Utilities
     [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
     public class InteractToggle : UdonSharpBehaviour 
     {
-        public PlaneResize plane;
+        //public PlaneResize plane;
         [Tooltip("List of objects to toggle on and off")]
         public GameObject toggleOffObjects;
         public GameObject toggleOnObjects;
 
         public override void Interact()
         {
-            plane.StartMoving();
+            //plane.StartMoving();
             toggleOffObjects.SetActive(false);
             toggleOnObjects.SetActive(true);
         }
